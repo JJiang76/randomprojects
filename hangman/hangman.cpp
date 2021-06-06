9db653195363b42a599999d829815c56b9152883
Jason <jasonjiang27@gmail.com>
2021-06-06 17:45:51 -0500

    Added wordlist file and parsing from it; added makefile

--- hangman/hangman.cpp
+++ hangman/hangman.cpp
@@ -1,3 +1,25 @@
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void play_game(const vector<string> &words) {
+	cout << "We playin" << endl;
+}
+
 int main() {
+	ifstream fin("words.txt");
+	vector<string> wordlist;
+	string s;
+
+	while (fin >> s) {
+		wordlist.push_back(s);
+	}
+
+	play_game(wordlist);
+
 	return 0;
 }
